b2c203ac9b68642611285ffd4d354860a4233082
vagrant <vagrant@vagrant.vm>
2019-05-08 03:49:27 +0000

    Create homepage and render it

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import (Flask, render_template, redirect, request, flash,
                     session)
-
+from model import connect_to_db 
 
 app = Flask(__name__)
 app.secret_key = 'abc'
@@ -9,5 +9,13 @@ app.secret_key = 'abc'
 def index(): 
     """ Homepage """
 
-    return print("Hello")
+    return render_template("homepage.html")
 
+
+
+if __name__ == "__main__": 
+    app.debug = True
+    connect_to_db(app)
+
+    app.run(port=5000, host='0.0.0.0')
+5000
