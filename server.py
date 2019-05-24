8112b2e21a830bf29e5fd05ac3b3bc42ecc4c986
vagrant <vagrant@vagrant.vm>
2019-05-24 21:13:52 +0000

    Added changes to accept ratings from user and allow user to signup

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import (Flask, render_template, redirect, request, flash, session)
 from model import connect_to_db, db, Route, Bus, User, User_Rating, Rating
 
+from werkzeug.security import generate_password_hash
+
 app = Flask(__name__)
 app.secret_key = 'abc'
 
@@ -88,10 +90,84 @@ def logout():
 
 
 @app.route('/signin', methods=['GET'])
-def user_signin(): 
+def signin_form(): 
     """ Render form for user signin"""
 
     return render_template("user_signin.html")
+
+
+@app.route('/signin', methods=['POST'])
+def user_signin(): 
+    """ Add new user """
+
+    email = request.form.get("email")
+
+    if User.query.filter(User.user_email == email).first(): 
+        flash("User already exsits. Login here")
+        print("User exists")
+        return redirect("/login")
+    else: 
+        fname = request.form.get("fname")
+        lname = request.form.get("lname")
+        pwd = request.form.get("password")
+
+        new_user = User(
+            user_fname = fname, 
+            user_lname = lname, 
+            user_email = email, 
+            user_pwd_hash = generate_password_hash(pwd)
+            )
+        db.session.add(new_user)
+        db.session.commit()
+        flash("Sign in successful")
+        print("Sign in success")
+
+        return redirect("/")
+
+
+@app.route('/rate_bus/<bus_id>', methods=['POST'])
+def rate_bus(bus_id):
+    """ Rate the bus id selected by user
+        If the user is logged in add rating to the ratings table and generate the
+        rating id and use that rating id load the user_rating table
+    """
+    print(bus_id)
+    if is_logged_in(): 
+        pass
+    else: 
+        flash("Please log in to rate this route")
+        return redirect("/login")
+
+    new_rating = Rating(
+        crowd_rating = int(request.form.get("crowd_rating")) ,
+        time_rating = int(request.form.get("time_rating")) ,
+        cleanliness_rating = int(request.form.get("cleanliness_rating")) ,
+        safety_rating = int(request.form.get("safety_rating")) ,
+        outer_view_rating = int(request.form.get("outer_view_rating")) ,
+        rating_text = request.form.get("rating_text"))
+    db.session.add(new_rating)
+    db.session.commit()
+    db.session.refresh(new_rating)
+    print(new_rating)
+
+    new_bus_rating = User_Rating(
+        user_id = session['user_id'], 
+        rating_id = new_rating.rating_id, 
+        bus_id = int(bus_id)
+        )
+    db.session.add(new_bus_rating)
+    db.session.commit()
+    return redirect("/")
+
+
+def is_logged_in(): 
+    """ Return True is a user is logged in. 
+        Else return False
+    """
+    return 'user_id' in session
+
+
+
 
 
 
